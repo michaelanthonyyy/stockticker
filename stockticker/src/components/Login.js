f0ad568be4042684f66629673be9ff44c46bcd98
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-04 11:45:43 -0600

    Update signup.js

--- stockticker/src/components/Login.js
+++ stockticker/src/components/Login.js
@@ -2,32 +2,30 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="container my-5">
-      <div className="row my-5">
-        <div className="col col-6 mx-auto">
-          <form>
-            <div className="form-group">
-              <label for="exampleInputEmail1">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-              />
-            </div>
-            <div className="form-group">
-              <label for="exampleInputPassword1">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="exampleInputPassword1"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
-        </div>
+    <div className="row my-5">
+      <div className="col col-6 mx-auto">
+        <form>
+          <div className="form-group">
+            <label for="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="form-group">
+            <label for="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
