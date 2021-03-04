1e025dcbde357938519d702cfa0891edf9308efd
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-03 22:25:59 -0600

    Switch css frameworks

--- stockticker/src/App.js
+++ stockticker/src/App.js
@@ -3,25 +3,8 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="">
-      <nav>
-        <div class="nav-wrapper">
-          <a href="#" class="brand-logo">
-            Logo
-          </a>
-          <ul id="nav-mobile" class="right hide-on-med-and-down">
-            <li>
-              <a href="sass.html">Sass</a>
-            </li>
-            <li>
-              <a href="badges.html">Components</a>
-            </li>
-            <li>
-              <a href="collapsible.html">JavaScript</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div classNameName="">
+      <h1>Hello</h1>
     </div>
   );
 }
