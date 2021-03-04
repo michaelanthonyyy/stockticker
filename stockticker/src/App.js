cb5e464d03cff567c1ee84f993c63ea34b01ca3d
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-04 08:01:23 -0600

    Add pages folder and update index.html

--- stockticker/src/App.js
+++ stockticker/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <div classNameName="">
       <h1>Hello</h1>
+      <Main />
     </div>
   );
 }
