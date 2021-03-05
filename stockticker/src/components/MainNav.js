1de7a28bda9a1fc84b63fa850added6d7f6df7c9
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-05 11:37:10 -0600

    New line added after save????

--- stockticker/src/components/MainNav.js
+++ stockticker/src/components/MainNav.js
@@ -11,7 +11,8 @@ const MainNav = () => {
         data-target="#navbarNavDropdown"
         aria-controls="navbarNavDropdown"
         aria-expanded="false"
-        aria-label="Toggle navigation">
+        aria-label="Toggle navigation"
+      >
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNavDropdown">
