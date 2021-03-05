a13b5da002bb6982cd8379ec96bb6a7438b2999d
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-05 16:18:30 -0600

    Add title component to Dashboard.js

---
+++ stockticker/src/components/Jumbotron.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Jumbotron = () => {
+  return (
+    <div class="container-fluid jumbo-container">
+      <div class="container jumbo-text-container">
+        <h4 class="display-4">Stock Dashboard</h4>
+        <p>
+          Welcome, <span>Username</span>!
+        </p>
+      </div>
+      <div className="container jumbo-form-container">
+        <form className="stock-form">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search for stocks"
+          />
+          <button className="btn btn-outline-primary">Search</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
--- stockticker/src/pages/Dashboard.js
+++ stockticker/src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Jumbotron from "../components/Jumbotron";
 import Nav from "../components/Nav";
 
 const Dashboard = () => {
   return (
     <>
       <Nav />
+      <Jumbotron />
     </>
   );
 };
