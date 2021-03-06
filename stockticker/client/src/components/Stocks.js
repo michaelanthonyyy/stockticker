32df90fa787c8028beded359e1c4a99aff75a172
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-06 15:44:01 -0600

    Update Stocks.js

--- stockticker/client/src/components/Stocks.js
+++ stockticker/client/src/components/Stocks.js
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Stocks = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="row">
+        <div className="col col-12">
+          <h4>Top Trending Daily Stocks / Chart / Optional </h4>
+        </div>
+
+        <div className="col col-4">
+          <div className="row">
+            <div className="col col-12">
+              <h4>Saved Stocks Container</h4>
+            </div>
+            <div className="col">
+              <h4>Optional Container</h4>
+            </div>
+          </div>
+        </div>
+        <div className="col col-8">
+          <h4>Chart / Stock Info</h4>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Stocks;
