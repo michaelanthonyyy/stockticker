62fe25b7a0eb3e19dc3c3b850ba1ff3583f890e1
Michael Medina <michaelanthony.medinaa@gmail.com>
2021-03-07 10:42:18 -0800

    added password confirmation on signup page

--- stockticker/client/src/components/Signup.js
+++ stockticker/client/src/components/Signup.js
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useRef } from "react";
 
-const Signup = () => {
+export default function Signup() {
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const passwordConfirmRef = useRef()
+
+// const Signup = () => {
   return (
     <div className="row">
       <div className="col col-6 mx-auto">
         <form>
           <div className="form-row">
-            <div className="form-group col-md-6">
+            {/* <div className="form-group col-md-6">
               <label for="inputFirstName">First Name</label>
               <input type="text" className="form-control" id="inputFirstName" />
             </div>
             <div className="form-group col-md-6">
               <label for="inputLastName">Last Name</label>
               <input type="text" className="form-control" id="inputLastName" />
-            </div>
-            <div className="form-group col-md-12">
+            </div> */}
+            <div className="form-group col-md-12" >
               <label for="inputEmail">Email</label>
-              <input type="email" className="form-control" id="inputEmail" />
+              <input type="email" className="form-control" id="inputEmail" ref={emailRef} required/>
             </div>
-            <div className="form-group col-md-6">
+            {/* <div className="form-group col-md-6">
               <label for="inputUserName">Username</label>
               <input type="text" className="form-control" id="inputUserName" />
-            </div>
+            </div> */}
             <div className="form-group col-md-6">
               <label for="inputPassword">Password</label>
               <input
                 type="password"
                 className="form-control"
                 id="inputPassword"
+                ref={passwordRef}
+                required
+              />
+            </div>
+            <div className="form-group col-md-6">
+              <label for="inputPassword">Confirm Password</label>
+              <input
+                type="passwordConfirm"
+                className="form-control"
+                id="passwordConfirm"
+                ref={passwordConfirmRef}
+                required
               />
             </div>
           </div>
           <button type="submit" className="btn btn-primary">
-            Sign in
+            Sign Up
           </button>
         </form>
       </div>
@@ -40,4 +57,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+// export default Signup;
