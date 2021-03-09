710e21360bb446f292507ba7331cf81bee3b17bd
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2021-03-09 17:29:28 -0600

    Formatting changes

--- stockticker/client/src/components/UserStock.js
+++ stockticker/client/src/components/UserStock.js
@@ -15,16 +15,14 @@ const UserStock = () => {
   useEffect(() => {
     console.log(userState);
     if (userLogin) {
-      API.getUserByEmail(userState.email)
-        .then((dbModel) => {
+      API.getUserByEmail(userState.email).then((dbModel) => {
         console.log(dbModel);
         if (dbModel.data) {
           setStockState(dbModel.data.stocks);
           setCommentState(dbModel.data.comments);
-  
+
           setUserLogin(false);
         }
-
       });
     }
   });
@@ -33,11 +31,12 @@ const UserStock = () => {
     e.preventDefault();
     var commentId = e.currentTarget.getAttribute("dataId");
     var textArea = e.target.children[0].children[0];
-    API.updateCommentById(commentId, {$set: {content: textArea.value} })
-      .then(dbModel => {
-        console.log(dbModel);
-        setUserLogin(true);
-      });
+    API.updateCommentById(commentId, {
+      $set: { content: textArea.value },
+    }).then((dbModel) => {
+      console.log(dbModel);
+      setUserLogin(true);
+    });
   }
 
   function handleDelete(e) {
@@ -46,12 +45,13 @@ const UserStock = () => {
     var textArea = e.target.parentNode.children[0].children[0];
 
     textArea.value = "";
-    API.updateCommentById(commentId, {$set: {content: ""} })
-    .then(dbModel => {
-      console.log(dbModel);
+    API.updateCommentById(commentId, { $set: { content: "" } }).then(
+      (dbModel) => {
+        console.log(dbModel);
 
-      setUserLogin(true);
-    });
+        setUserLogin(true);
+      }
+    );
   }
 
   // var comment="";
@@ -74,31 +74,48 @@ const UserStock = () => {
         {stockState.map((stock) => {
           var dataId = 0;
           var indexId = 0;
-          for (let i=0; i<stockState.length; i++) {
-            if (commentState[i]) {if (commentState[i].ticker === stock) {
-              dataId = commentState[i]._id;
-              indexId = i;
-            }}
+          for (let i = 0; i < stockState.length; i++) {
+            if (commentState[i]) {
+              if (commentState[i].ticker === stock) {
+                dataId = commentState[i]._id;
+                indexId = i;
+              }
+            }
           }
           return (
-          <li>
-            <Graph ticker={stock} />
-            <form dataId={dataId} onSubmit={handleFormSubmit}>
-            <div className="form-group">
-            <textarea
-              type="text"
-              placeholder="Add a comment"
-              className="form-control"
-              index={indexId} value={commentState[indexId] ? commentState[indexId].content : ""} onChange={handleInputChange} />
-            </div>
-            <button dataId={dataId} type="submit" className="btn btn-primary">
-            Save
-          </button>
-          <button dataId={dataId} onClick={handleDelete} className="btn btn-danger">X</button>
-            </form>
-          </li>
-          
-        )})}
+            <li>
+              <Graph ticker={stock} />
+              <form dataId={dataId} onSubmit={handleFormSubmit}>
+                <div className="form-group">
+                  <textarea
+                    type="text"
+                    placeholder="Add a comment"
+                    className="form-control"
+                    index={indexId}
+                    value={
+                      commentState[indexId] ? commentState[indexId].content : ""
+                    }
+                    onChange={handleInputChange}
+                  />
+                </div>
+                <button
+                  dataId={dataId}
+                  type="submit"
+                  className="btn btn-primary"
+                >
+                  Save
+                </button>
+                <button
+                  dataId={dataId}
+                  onClick={handleDelete}
+                  className="btn btn-danger"
+                >
+                  X
+                </button>
+              </form>
+            </li>
+          );
+        })}
       </ul>
 
       {/* <ul>
